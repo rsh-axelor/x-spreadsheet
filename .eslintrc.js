ef62ad3597d38488570876baf76441a4792557f6
r.shah <r.shah@axelor.com>
2021-02-19 18:26:19 +0530

    Update eslist

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "rules": {
-    "no-param-reassign": ["error", { "props": false }],
-    "class-methods-use-this": "off",
-  },
+  parser: "babel-eslint",
 };
